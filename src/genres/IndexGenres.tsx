3b901c333838bd8f1d04ab8d551e2ce00fc3c551
codeeztech <srk.shahroz63@gmail.com>
2022-08-25 15:35:52 +0400

    axios added

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,6 +1,19 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import {useEffect} from 'react'
 import { Link } from "react-router-dom";
+import { genreDTO } from './genres.model';
 
 export default function IndexGenres(){
+
+useEffect(() =>{
+axios.get(`https://localhost:7212/api/Genre`).then((response : AxiosResponse<genreDTO[]>)=>{
+    console.log(response.data)
+}).then((error) =>{
+    console.log(error)
+});
+
+});
+
     return (
         <>
             <h3>Genres</h3>
